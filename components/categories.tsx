cf218455988949e4af77d1a977645a15bd1df90e
ArpitaGanatra <ganatraarpita@gmail.com>
2025-03-15 11:41:23 +0400

    fix: changes

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import React from "react";
 import { useCategory } from "@/app/context/CategoryContext";
 import { podcastData } from "@/lib/podcast-data";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Categories() {
   const { selectedCategory, setSelectedCategory } = useCategory();
   const pathname = usePathname();
-
+  const router = useRouter();
   // Calculate counts
   const counts = {
     all: podcastData.length,
@@ -22,9 +21,75 @@ export default function Categories() {
   return (
     <div
       className={`${
-        pathname === "/" ? "flex" : "hidden"
-      } md:flex flex-col gap-0.5 items-end absolute bottom-0.5 right-0.5`}
+        pathname === "/" || pathname === "/index-page" ? "flex" : "hidden"
+      } flex md:flex-col gap-0.5 items-stretch md:items-end absolute left-1 z-[9999] w-screen bottom-0.5 right-0.5 overflow-scroll`}
     >
+      <button
+        className={`md:hidden button rounded-lg px-3 py-2 bg-gray-100 border border-gray-200 text-xs text-black hover:text-opacity-[1]`}
+        onClick={() => {
+          if (pathname === "/index-page") {
+            router.push("/");
+          } else {
+            router.push("/index-page");
+          }
+        }}
+      >
+        {pathname === "/index-page" ? (
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-4 h-4"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M16.9498 8.46447C17.3404 8.07394 17.3404 7.44078 16.9498 7.05025C16.5593 6.65973 15.9261 6.65973 15.5356 7.05025L12.0001 10.5858L8.46455 7.05025C8.07402 6.65973 7.44086 6.65973 7.05033 7.05025C6.65981 7.44078 6.65981 8.07394 7.05033 8.46447L10.5859 12L7.05033 15.5355C6.65981 15.9261 6.65981 16.5592 7.05033 16.9497C7.44086 17.3403 8.07402 17.3403 8.46455 16.9497L12.0001 13.4142L15.5356 16.9497C15.9261 17.3403 16.5593 17.3403 16.9498 16.9497C17.3404 16.5592 17.3404 15.9261 16.9498 15.5355L13.4143 12L16.9498 8.46447Z"
+                fill="#000000"
+              ></path>{" "}
+            </g>
+          </svg>
+        ) : (
+          <svg
+            fill="#000000"
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 276.167 276.167"
+            className="w-[10px] h-[10px]"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <g>
+                <g>
+                  <path d="M33.144,2.471C15.336,2.471,0.85,16.958,0.85,34.765s14.48,32.293,32.294,32.293s32.294-14.486,32.294-32.293 S50.951,2.471,33.144,2.471z"></path>{" "}
+                  <path d="M137.663,2.471c-17.807,0-32.294,14.487-32.294,32.294s14.487,32.293,32.294,32.293c17.808,0,32.297-14.486,32.297-32.293 S155.477,2.471,137.663,2.471z"></path>{" "}
+                  <path d="M243.873,67.059c17.804,0,32.294-14.486,32.294-32.293S261.689,2.471,243.873,2.471s-32.294,14.487-32.294,32.294 S226.068,67.059,243.873,67.059z"></path>{" "}
+                  <path d="M32.3,170.539c17.807,0,32.297-14.483,32.297-32.293c0-17.811-14.49-32.297-32.297-32.297S0,120.436,0,138.246 C0,156.056,14.493,170.539,32.3,170.539z"></path>{" "}
+                  <path d="M136.819,170.539c17.804,0,32.294-14.483,32.294-32.293c0-17.811-14.478-32.297-32.294-32.297 c-17.813,0-32.294,14.486-32.294,32.297C104.525,156.056,119.012,170.539,136.819,170.539z"></path>{" "}
+                  <path d="M243.038,170.539c17.811,0,32.294-14.483,32.294-32.293c0-17.811-14.483-32.297-32.294-32.297 s-32.306,14.486-32.306,32.297C210.732,156.056,225.222,170.539,243.038,170.539z"></path>{" "}
+                  <path d="M33.039,209.108c-17.807,0-32.3,14.483-32.3,32.294c0,17.804,14.493,32.293,32.3,32.293s32.293-14.482,32.293-32.293 S50.846,209.108,33.039,209.108z"></path>{" "}
+                  <path d="M137.564,209.108c-17.808,0-32.3,14.483-32.3,32.294c0,17.804,14.487,32.293,32.3,32.293 c17.804,0,32.293-14.482,32.293-32.293S155.368,209.108,137.564,209.108z"></path>{" "}
+                  <path d="M243.771,209.108c-17.804,0-32.294,14.483-32.294,32.294c0,17.804,14.49,32.293,32.294,32.293 c17.811,0,32.294-14.482,32.294-32.293S261.575,209.108,243.771,209.108z"></path>{" "}
+                </g>
+              </g>
+            </g>
+          </svg>
+        )}
+      </button>
       {[
         { name: "All", count: counts.all, path: "/" },
         { name: "Founders", count: counts.founders, path: "/founders" },
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,13 +11,13 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="flex items-center gap-0.5 absolute left-0.5 top-0.5">
+      <div className="flex items-center gap-0.5 absolute left-0.5 top-0.5 right-0.5 w-full pr-1">
         <button
           onClick={() => {
             setIsOpen(false);
             router.push("/");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
+          className={`z-20 button text-[10px] uppercase flex w-full md:w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
             !isOpen && pathname === "/"
               ? "text-opacity-[1]"
               : "text-opacity-[0.3]"
@@ -38,7 +38,7 @@ export default function Navbar() {
             setIsOpen(false);
             router.push("/about");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
+          className={`z-20 button text-[10px] uppercase flex w-full md:w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
             !isOpen && pathname === "/about"
               ? "text-opacity-[1]"
               : "text-opacity-[0.3]"
@@ -55,7 +55,7 @@ export default function Navbar() {
           </span>
         </button>
         <button
-          className={`z-20 button text-[10px] uppercase flex w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
+          className={`z-20 button text-[10px] uppercase flex w-full md:w-fit bg-gray-100 border border-gray-200 items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black ${
             isOpen ? "text-opacity-[1]" : "text-opacity-[0.3]"
           } hover:text-opacity-[1]`}
           onClick={() => setIsOpen(true)}
