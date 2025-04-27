f800cf5260c6c0fcfce1421a9148bfcbeb3668dc
ArpitaGanatra <ganatraarpita@gmail.com>
2025-04-28 02:35:41 +0530

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import MobileView from "./components/mobile-view";
 import WebView from "./components/web-view";
 
 const Home = () => {
-  if (window.innerWidth < 768) {
-    return <MobileView />;
-  } else {
-    return <WebView />;
-  }
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Initial check
+    checkMobile();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", checkMobile);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  return isMobile ? <MobileView /> : <WebView />;
 };
 
 export default Home;
