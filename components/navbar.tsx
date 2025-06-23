8d8bb6f9bb5e92350b97c599fdffdecd3e7d0d55
ArpitaGanatra <ganatraarpita@gmail.com>
2025-06-23 10:24:11 +0530

    fix navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
             setIsOpen(false);
             router.push("/");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black 
+          className={`z-20 button text-[10px] uppercase flex w-fit flex-grow md:flex-grow-0 whitespace-nowrap items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-3 md:pr-10 py-2 pt-10 text-black 
           backdrop-blur-[16px] backdrop-saturate-[180%] bg-[#F2F2F2]/40 border border-[#D3D3D3]/50 hover:bg-[#F2F2F2]/50 transition-all duration-300 
           ${
             !isOpen && pathname === "/"
@@ -41,7 +41,7 @@ export default function Navbar() {
             setIsOpen(false);
             router.push("/lore");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black 
+          className={`z-20 button text-[10px] uppercase flex w-fit flex-grow md:flex-grow-0 whitespace-nowrap items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-3 md:pr-10 py-2 pt-10 text-black 
           backdrop-blur-[16px] backdrop-saturate-[180%] bg-[#F2F2F2]/40 border border-[#D3D3D3]/50 hover:bg-[#F2F2F2]/50 transition-all duration-300 
           ${
             !isOpen && pathname === "/lore"
@@ -64,7 +64,7 @@ export default function Navbar() {
             setIsOpen(false);
             router.push("/rsdnts");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black 
+          className={`z-20 button text-[10px] uppercase flex w-fit flex-grow md:flex-grow-0 whitespace-nowrap items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-3 md:pr-10 py-2 pt-10 text-black 
           backdrop-blur-[16px] backdrop-saturate-[180%] bg-[#F2F2F2]/40 border border-[#D3D3D3]/50 hover:bg-[#F2F2F2]/50 transition-all duration-300 
           ${
             !isOpen && pathname === "/rsdnts"
@@ -88,7 +88,7 @@ export default function Navbar() {
             setIsOpen(false);
             router.push("/about");
           }}
-          className={`z-20 button text-[10px] uppercase flex w-fit items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black 
+          className={`z-20 button text-[10px] uppercase flex w-fit flex-grow md:flex-grow-0 whitespace-nowrap items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-3 md:pr-10 py-2 pt-10 text-black 
           backdrop-blur-[16px] backdrop-saturate-[180%] bg-[#F2F2F2]/40 border border-[#D3D3D3]/50 hover:bg-[#F2F2F2]/50 transition-all duration-300 
           ${
             !isOpen && pathname === "/about"
@@ -108,7 +108,7 @@ export default function Navbar() {
         </button>
 
         <button
-          className={`z-20 button text-[10px] uppercase flex w-fit items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-10 py-2 pt-10 text-black 
+          className={`z-20 button text-[10px] uppercase flex w-fit flex-grow md:flex-grow-0 whitespace-nowrap items-start gap-x-[2px] rounded-[0.375rem] pl-2.5 pr-3 md:pr-10 py-2 pt-10 text-black 
           backdrop-blur-[16px] backdrop-saturate-[180%] bg-[#F2F2F2]/40 border border-[#D3D3D3]/50 hover:bg-[#F2F2F2]/50 transition-all duration-300 
           ${
             isOpen && pathname === "/contact"
