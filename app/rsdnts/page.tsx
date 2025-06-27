28b249bc1a308101619f4fc5f6a1bb350c74cad2
ArpitaGanatra <ganatraarpita@gmail.com>
2025-06-27 20:28:08 +0530

    coming soon

--- app/rsdnts/page.tsx
+++ app/rsdnts/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 export default function Rsdnts() {
   return (
     <div className="min-h-screen w-full flex justify-center items-center  text-white ">
-      <div className="max-w-2xl w-full px-8  md:px-12 bg-white/5  text-center">
-        <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-white to-gray-400 bg-clip-text text-transparent tracking-tight py-8 leading-tight">
+      <div className="max-w-2xl w-full px-8  md:px-12 bg-white/5 items-center flex flex-col  text-center">
+        <Image src="/rsdnts.png" alt="rsdnts" width={300} height={300} />
+        <h1 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-gray-200 to-gray-400 bg-clip-text text-transparent tracking-tight py-8 leading-tight">
           Coming Soon
         </h1>
       </div>
