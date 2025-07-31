2b54413b08c9db4ada97bea7e925c05f328e8316
ArpitaGanatra <ganatraarpita@gmail.com>
2025-07-31 19:19:54 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Crypotown",
+  title: "Cryptotown",
   description: "a curated crypto network",
   icons: {
     icon: "/logos/logo.png",
